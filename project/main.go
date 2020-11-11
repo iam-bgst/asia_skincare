0eabe46e9948b77017747c1ef3ee1b3653b6eed1
gusta61 <bimagusta61@gmail.com>
2020-11-11 14:27:33 +0700

    update

--- project/main.go
+++ project/main.go
@@ -33,10 +33,10 @@ func init() {
 	models.InitialAssets()
 
 	// Service Point
-	// models.ServicePoint()
+	models.ServicePoint()
 
 	// Service Transaction
-	// models.ServiceTransaction()
+	models.ServiceTransaction()
 
 }
 
